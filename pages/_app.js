4e9fb84f7056dd723320d9351f490d8223308bf0
ewill025 <ericwilliamsjr@live.com>
2022-08-11 02:28:12 -0400

    initial

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import { config } from "@fortawesome/fontawesome-svg-core";
+import "@fortawesome/fontawesome-svg-core/styles.css";
+config.autoAddCss = false;
+import { SessionProvider } from "next-auth/react";
+import { ContextProvider } from "../context/ContextProvider";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps: { session, ...pageProps } }) {
+  return (
+    <SessionProvider session={session}>
+      <ContextProvider>
+        <Component {...pageProps} />
+      </ContextProvider>
+    </SessionProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
